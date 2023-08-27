a067e46969bb028ab6f525d83066e62f6fb96621
Daniel 'Vector' Kerr <vector.kerr@gmail.com>
2023-08-27 12:37:57 +1000

    feat: add documentation

--- src/gqrx_client/transport/transport.py
+++ src/gqrx_client/transport/transport.py
@@ -5,6 +5,9 @@ class Transport:
     def __init__(self):
         self.log = logging.getLogger(__name__)
 
+    def close(self) -> None:
+        raise NotImplementedError()
+
     def send(self, message: bytes) -> None:
         raise NotImplementedError()
 
